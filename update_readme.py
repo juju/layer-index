af9f535f6666784977ccdc9d7d687d3c5fc0d523
Marco Ceppi <marco@ceppi.net>
2017-09-08 11:28:48 -0400

    Insert newlines around markers (#3)

--- update_readme.py
+++ update_readme.py
@@ -10,12 +10,14 @@ for layer_type in ('layers', 'interfaces'):
     start = readme.index('<!-- list-of-{} -->'.format(layer_type))
     end = readme.index('<!-- /list-of-{} -->'.format(layer_type))
     del readme[start+1:end]
-    readme.insert(start+1, '| ID | Name | Summary |')
-    readme.insert(start+2, '| --- | --- | --- |')
+    readme.insert(start+1, '')
+    readme.insert(start+2, '| ID | Name | Summary |')
+    readme.insert(start+3, '| --- | --- | --- |')
     for layer_file in sorted(Path(layer_type).glob('*.json')):
         end = readme.index('<!-- /list-of-{} -->'.format(layer_type))
         layer = json.loads(layer_file.read_text())
         readme.insert(
             end, '| [{id}]({repo}) | {name} | {summary} |'.format(**layer))
+    readme.insert(end+1, '')
 
 readme_file.write_text(''.join('{}\n'.format(l) for l in readme))
