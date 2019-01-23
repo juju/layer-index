e17288a7da6c7352a391221f8cf9eeb32aa718e9
Cory Johns <johnsca@gmail.com>
2019-01-23 18:01:54 -0500

    Add explicit repo and docs links to README
    Add ability to specify a docs link separately from the repo.
    
    ref: https://discourse.jujucharms.com/t/layer-apt-documentation/501

--- update_readme.py
+++ update_readme.py
@@ -2,6 +2,30 @@
 
 import json
 from pathlib import Path
+from urllib.parse import urlparse, urljoin, urlunparse
+
+
+def set_default_docs_link(layer):
+    if 'docs' in layer:
+        return
+    repo = layer['repo']
+    repo_parsed = urlparse(repo)
+    netloc = repo_parsed.netloc
+    docs = None
+    if netloc == 'github.com':
+        docs = urljoin(repo, '#readme')
+    elif netloc == 'git.launchpad.net':
+        docs = urljoin(repo, 'tree/README.md')
+    elif netloc in ('code.launchpad.net', 'launchpad.net'):
+        docs = urljoin(
+            urlunparse(repo_parsed._replace(netloc='bazaar.launchpad.net')),
+            'files/README.md')
+    elif netloc == 'bazaar.launchpad.net':
+        docs = urljoin(repo, 'files/README.md')
+    else:
+        docs = repo
+    layer['docs'] = docs
+
 
 readme_file = Path('README.md')
 readme = readme_file.read_text().splitlines()
@@ -11,13 +35,18 @@ for layer_type in ('layers', 'interfaces'):
     end = readme.index('<!-- /list-of-{} -->'.format(layer_type))
     del readme[start+1:end]
     readme.insert(start+1, '')
-    readme.insert(start+2, '| ID | Name | Summary |')
-    readme.insert(start+3, '| --- | --- | --- |')
+    readme.insert(start+2, '| ID | Repo | Docs | Name | Summary |')
+    readme.insert(start+3, '| --- | --- | --- | --- | --- |')
     for layer_file in sorted(Path(layer_type).glob('*.json')):
         end = readme.index('<!-- /list-of-{} -->'.format(layer_type))
         layer = json.loads(layer_file.read_text())
-        readme.insert(
-            end, '| [{id}]({repo}) | {name} | {summary} |'.format(**layer))
+        set_default_docs_link(layer)
+        readme.insert(end,
+                      '| {id} '
+                      '| [Repo]({repo}) '
+                      '| [Docs]({docs}) '
+                      '| {name} '
+                      '| {summary} |'.format(**layer))
     readme.insert(end+1, '')
 
 readme_file.write_text(''.join('{}\n'.format(l) for l in readme))
